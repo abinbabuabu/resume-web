ede189f336bb72159598f6d01bcebee09d5cc2d8
Abin Babu <abinbabuabk@gmail.com>
2024-01-19 01:41:20 +0530

    Initial Hero Section

---
+++ app/about/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section className="lg:pr-16 md:pr-8 sm:pr-4 sm:pl-4 md:pl-0">
+            <div>
+                <p>I use code to solve real-world problems :-)</p>
+
+                <p>
+                    I started my journey as a software engineer while pursuing my bachelor's degree, met
+                    few amazing people there and we bootstrapped{' '}
+                    <a href="https://www.emildasolutions.com/">emilda solutions</a>, to solve customer
+                    problems through web & mobile solutions. Fast-forward to today, I'm helping the world
+                    run better with <a href="https://www.sap.com/india/index.html">SAP</a>.
+                </p>
+
+                <p>
+                    In my <a href="/#jobs">career</a>, I've worn multiple hats as a mobile developer,
+                    backend developer, and frontend developer. Passion for tinkering and figuring out new
+                    things keeps me moving...
+                </p>
+
+                <p>Here are a few technologies I’ve been working with recently:</p>
+            </div>
+        </section>
+    )
+}
---
+++ app/components/hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <main className="lg:pl-16 md:pl-8 sm:pl-4" >
+            <header>
+                <h4 className="my-2 text-base">Hello I&apos;m</h4>
+                <h2 className="my-2 text-5xl font-bold">Abin Babu</h2>
+                <p className="">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry‘s standard dummy text ever since the 1500s,
+                </p>
+
+
+            </header>
+        </main>
+    )
+}
